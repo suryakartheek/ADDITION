74cdc3595f4f57779fd2926e379b0e077187a7fc
suryakartheek <140693086+suryakartheek@users.noreply.github.com>
2025-01-29 11:15:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
  function calculateSum() {
-            const num1 = parseFloat(document.getElementById('num1').value) || 0;
-            const num2 = parseFloat(document.getElementById('num2').value) || 0;
+            const num1 = parseFloat(document.getElementById('num1').value);
+            const num2 = parseFloat(document.getElementById('num2').value);
             document.getElementById('result').textContent = `Result: ${num1 + num2}`;
         }
